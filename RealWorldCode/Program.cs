41a1db17047916a4721aa04414f9b618bd58f405
Kirsty Selesho <KSelesho@you-source.co.za>
2022-07-21 14:11:35 +0200

    Abstract Factory Design Pattern

---
+++ RealWorldCode/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace RealWorldCode
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            ContinentFactory africa = new AfricaFactory();
+            AnimalWorld world = new AnimalWorld(africa);
+            world.RunFoodChain();
+
+            ContinentFactory america = new AmericaFactory();
+            AnimalWorld world1 = new AnimalWorld(america);
+            world1.RunFoodChain();
+
+        }
+    }
+
+    abstract class ContinentFactory
+    {
+        public abstract Herbivore CreateHerbivore();
+        public abstract Carnivore CreateCarnivore();
+    }
+
+    class AfricaFactory : ContinentFactory
+    {
+        public override Carnivore CreateCarnivore()
+        {
+            return new Lion();
+        }
+
+        public override Herbivore CreateHerbivore()
+        {
+            return new Wildebeest();
+        }
+    }
+
+    class AmericaFactory : ContinentFactory
+    {
+        public override Carnivore CreateCarnivore()
+        {
+            return new Wolf();
+        }
+
+        public override Herbivore CreateHerbivore()
+        {
+            return new Bison();
+        }
+    }
+
+    abstract class Herbivore
+    {
+
+    }
+
+    abstract class Carnivore
+    {
+        public abstract void Eat(Herbivore h);
+    }
+
+    class Wildebeest: Herbivore
+    {
+
+    }
+
+    class Lion : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            Console.WriteLine(this.GetType().Name + 
+                " " + "eats" + " " + h.GetType().Name + "\n");
+        }
+    }
+
+    class Bison : Herbivore
+    {
+
+    }
+
+    class Wolf: Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+           Console.Write(this.GetType().Name + " " 
+               + "eats" + " " + h.GetType().Name + "\n");
+        }
+    }
+
+    class AnimalWorld
+    {
+        private Herbivore _herbivore;
+        private Carnivore _carnivore;
+
+        public AnimalWorld(ContinentFactory factory)
+        {
+            _carnivore = factory.CreateCarnivore();
+            _herbivore = factory.CreateHerbivore();
+        }
+
+        public void RunFoodChain()
+        {
+            _carnivore.Eat(_herbivore);
+        }
+    }
+}
---
+++ StructuralCode/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace StructuralCode
+{
+    class MainApp
+    {
+        public static void Main(string[] args)
+        {
+            AbstractFactory factory1 = new ConcreteFactory1();
+            Client client1 = new Client(factory1);
+            client1.Run();
+
+            AbstractFactory factory2 = new ConcreteFactory2();
+            Client client2 = new Client(factory2);
+            client2.Run(); 
+        }
+    }
+
+    //declares an interface for operations that create abstract products
+    abstract class AbstractFactory
+    {
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+
+    //implements the operations to create concreate products
+    class ConcreteFactory1 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+
+    //implements the operations to create concreate products
+
+    class ConcreteFactory2 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA2();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB2();
+        }
+    }
+
+    //declares an interface for a type of product object 
+    abstract class AbstractProductA
+    {
+
+    }
+
+    //declares an interface for a type of product object 
+    abstract class AbstractProductB
+    {
+        public abstract void Interact(AbstractProductA a);  
+    }
+
+    //Defines a product object to be created by the correspoinding concrete
+    //factory
+    //implements the AbstractProduct Interface
+
+    class ProductA1 : AbstractProductA
+    {
+
+    }
+
+    //Defines a product object to be created by the correspoinding concrete
+    //factory
+    //implements the AbstractProduct Interface
+    class ProductB1 : AbstractProductB
+    {
+        public override void Interact(AbstractProductA a)
+        {
+            Console.WriteLine(this.GetType().Name + " " +
+                "interacts with" + " " + a.GetType().Name + "\n");
+        }
+    }
+
+    //Defines a product object to be created by the correspoinding concrete
+    //factory
+    //implements the AbstractProduct Interface
+    class ProductA2 : AbstractProductA
+    {
+
+    }
+
+    //Defines a product object to be created by the correspoinding concrete
+    //factory
+    //implements the AbstractProduct Interface
+    class ProductB2 : AbstractProductB
+    {
+        public override void Interact(AbstractProductA a)
+        {
+            Console.WriteLine(this.GetType().Name + " " +
+                "interacts with" + " " + a.GetType().Name);
+        }
+    }
+
+    //Uses interfaces declared by AbstractFactory and AbstractProduct classes 
+    class Client
+    {
+        private AbstractProductA _abstractProductA;
+        private AbstractProductB _abstractProductB;
+
+        public Client(AbstractFactory factory)
+        {
+            _abstractProductB = factory.CreateProductB();
+            _abstractProductA = factory.CreateProductA();
+        }
+
+        public void Run()
+        {
+            _abstractProductB.Interact(_abstractProductA);
+        }
+    }
+}
